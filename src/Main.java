602a9d7d210cf5d4c2f277cc3ceb541b325963e1
jaimeperezrisco <jaimepimpampum@gmail.com>
2025-03-31 12:44:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        ArrayList<Mundo>listaPokemonMundo =new ArrayList<>();
+
+        ArrayList <Pokemon> listaPokemon = new ArrayList<>();
+        list.add(charmander)//hay que previmanete crear el pokemon
+    }
+}
