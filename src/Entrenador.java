981b24412d8faa82144af5aeac936a54cd4e53ea
jaimeperezrisco <jaime.perez00vsl@gmail.com>
2025-04-04 13:13:02 +0000

    cambio en algunos metodos

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Entrenador {
-    
-   private int pokeball;
+
+    private int pokeball;
 
     public int getPokeball() {
         return pokeball;
@@ -10,13 +12,18 @@ public class Entrenador {
         this.pokeball = pokeball;
     }
 
-    //constructor por defecto, entrenador aparece con 6 pokeballs
+    // constructor por defecto, entrenador aparece con 6 pokeballs
     public Entrenador() {
         this.pokeball = 6;
     }
 
     public boolean capturar(Pokemon pokemon) {
 
+        int numeroPokeballs = this.getPokeball();
+        if (numeroPokeballs == 0) {
+            System.out.println("No puedes capturar más pokemons, te quedan 0 Pokeballs");
+            return false;
+        }
         double probabilidadPokeball = 0.5;
 
         double probabilidadCaputura = probabilidadPokeball * (pokemon.getVida() / pokemon.getVidaMaxima());
@@ -28,5 +35,28 @@ public class Entrenador {
         } else {
             return false;
         }
+
     }
+
+    public void añadirPokedex(Entrenador JaimeKetchup, ArrayList<Pokemon> listaPokemon,
+            ArrayList<Pokemon> listaPokemonMundo, int index) {
+        if (listaPokemonMundo.isEmpty()) {
+            System.out.println("No se puede quedan no Pokemons por capturar");
+        }
+
+        Pokemon pokemonSeleccionado = listaPokemonMundo.get(index);
+
+        if (JaimeKetchup.capturar(pokemonSeleccionado) == true) {
+            listaPokemon.add(pokemonSeleccionado);
+            listaPokemonMundo.remove(index);
+            System.out.println(
+                    "Se ha capturado el pokemon " + pokemonSeleccionado.getNombre() + " y se ha añadido a la Pokedex");
+        } else {
+            System.out.println("Captura fallida se ha gastado una pokeball");
+            JaimeKetchup.setPokeball(JaimeKetchup.getPokeball() - 1);
+
+            System.out.println("Te quedan " + JaimeKetchup.getPokeball());
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 
         // Tipo Fuego
         PokemonFuego Charizar = new PokemonFuego("Charizar", 50, 100, 100, 23);
-
         PokemonFuego Arcanine = new PokemonFuego("Arcanine", 55, 100, 100, 30);
         PokemonFuego Rapidash = new PokemonFuego("Rapidash", 50, 100, 100, 27);
         PokemonFuego Magmar = new PokemonFuego("Magmar", 45, 100, 100, 25);
@@ -14,7 +13,6 @@ public class Main {
         // Tipo Agua
 
         PokemonAgua Squirtle = new PokemonAgua("Squirtle", 15, 100, 100, 10);
-
         PokemonAgua Blastoise = new PokemonAgua("Blastoise", 60, 100, 100, 36);
         PokemonAgua Gyarados = new PokemonAgua("Gyarados", 65, 100, 100, 35);
         PokemonAgua Vaporeon = new PokemonAgua("Vaporeon", 55, 100, 100, 28);
@@ -23,66 +21,81 @@ public class Main {
         // Tipo planta
 
         PokemonPlanta Bulbasur = new PokemonPlanta("Bulbasur", 10, 100, 100, 15);
-
         PokemonPlanta Venusaur = new PokemonPlanta("Venusaur", 65, 100, 100, 32);
         PokemonPlanta Victreebel = new PokemonPlanta("Victreebel", 55, 100, 100, 30);
 
+        //Entrenador
         Entrenador JaimeKetchup = new Entrenador();
 
+
+
+        //Lista con los Pokemons que hay en el mundo
         ArrayList<Pokemon> listaPokemonMundo = new ArrayList<>();
-        listaPokemonMundo.add(Venusaur);
-        listaPokemonMundo.add(Blastoise);
-        listaPokemonMundo.add(Flareon);
-        listaPokemonMundo.add(Arcanine);
-        listaPokemonMundo.add(Poliwrath);
-        listaPokemonMundo.add(Magmar);
+        listaPokemonMundo.add(Venusaur); // index 0
+        listaPokemonMundo.add(Blastoise); // index 1
+        listaPokemonMundo.add(Flareon); // index 2
+        listaPokemonMundo.add(Arcanine); // index 3
+        listaPokemonMundo.add(Poliwrath); // index 4
+        listaPokemonMundo.add(Magmar); // index 5
 
+
+        //Lista de la pokedex de Jaime
         ArrayList<Pokemon> listaPokemon = new ArrayList<>();
-        listaPokemon.add(Charizar);
-        listaPokemon.add(Rapidash);
-        listaPokemon.add(Vaporeon);
-        listaPokemon.add(Gyarados);
-        listaPokemon.add(Bulbasur);
-        listaPokemon.add(Squirtle);
+        listaPokemon.add(Charizar); // index 0
+        listaPokemon.add(Rapidash); // index 1
+        listaPokemon.add(Vaporeon); // index 2
+        listaPokemon.add(Gyarados); // index 3
+        listaPokemon.add(Bulbasur); // index 4
+        listaPokemon.add(Squirtle); // index 5
 
-        // pelea fuego vs planta
-        System.out.println(
-                "Combate entre " + listaPokemon.get(0).getNombre() + " y " + listaPokemonMundo.get(0).getNombre());
-        System.out.println(listaPokemon.get(0));
-        System.out.println(listaPokemonMundo.get(0));
+        
 
-        listaPokemon.get(0).atacar(listaPokemonMundo.get(0));
+        /*
+         * pelea fuego vs planta
+         * System.out.println("Combate entre " + listaPokemon.get(0).getNombre() + " y "
+         * + listaPokemonMundo.get(0).getNombre());
+         * System.out.println(listaPokemon.get(0));
+         * System.out.println(listaPokemonMundo.get(0));
+         * 
+         * listaPokemon.get(0).atacar(listaPokemonMundo.get(0));
+         * 
+         * System.out.println(listaPokemon.get(0));
+         * System.out.println(listaPokemonMundo.get(0));
+         * 
+         * System.out.println();
+         * 
+         * // planta vs agua
+         * System.out.println(
+         * "Combate entre " + listaPokemon.get(4).getNombre() + " y " +
+         * listaPokemonMundo.get(0).getNombre());
+         * 
+         * System.out.println(listaPokemon.get(4));
+         * System.out.println(listaPokemonMundo.get(0));
+         * 
+         * listaPokemon.get(4).atacar(listaPokemonMundo.get(0));
+         * 
+         * System.out.println(listaPokemon.get(4));
+         * System.out.println(listaPokemonMundo.get(0));
+         * 
+         * listaPokemon.get(4).atacar(listaPokemonMundo.get(0));
+         * 
+         * System.out.println(listaPokemon.get(4));
+         * System.out.println(listaPokemonMundo.get(0));
+         * 
+         * listaPokemon.get(4).atacar(listaPokemonMundo.get(0));
+         * 
+         * System.out.println(listaPokemon.get(4));
+         * System.out.println(listaPokemonMundo.get(0));
+         */
+        // Captura
+        JaimeKetchup.añadirPokedex(JaimeKetchup, listaPokemon, listaPokemonMundo, 0);
+        JaimeKetchup.añadirPokedex(JaimeKetchup, listaPokemon, listaPokemonMundo, 1);
+        JaimeKetchup.añadirPokedex(JaimeKetchup, listaPokemon, listaPokemonMundo, 2);
+        JaimeKetchup.añadirPokedex(JaimeKetchup, listaPokemon, listaPokemonMundo,3);
 
-        System.out.println(listaPokemon.get(0));
-        System.out.println(listaPokemonMundo.get(0));
+        System.out.println(listaPokemon);
 
-        System.out.println();
-
-        // planta vs agua
-        System.out.println(
-                "Combate entre " + listaPokemon.get(4).getNombre() + " y " + listaPokemonMundo.get(0).getNombre());
-
-        System.out.println(listaPokemon.get(0));
-        System.out.println(listaPokemonMundo.get(4));
-
-        listaPokemon.get(4).atacar(listaPokemonMundo.get(0));
-
-        System.out.println(listaPokemon.get(4));
-        System.out.println(listaPokemonMundo.get(0));
-
-        if (JaimeKetchup.capturar(listaPokemonMundo.get(0)) == true) {
-            listaPokemon.add(listaPokemonMundo.get(0));
-            listaPokemonMundo.remove(listaPokemonMundo.get(0));
-            System.out.println("Se ha capturado el pokemon " + listaPokemonMundo.get(0).getNombre() + " y se ha añadido a la Pokedex");
-            // System.out.println(listaPokemonMundo);
-
-            // System.out.println(listaPokemon);
-        } else {
-            System.out.println("Captura fallida se ha gastado una pokeball");
-            JaimeKetchup.setPokeball(JaimeKetchup.getPokeball() - 1);
-            
-            System.out.println("Te quedan " + JaimeKetchup.getPokeball());
-        }
-
+        
     }
+
 }
